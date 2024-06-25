0e2dece1b2eac4bc959c280a5e66bf795d391c5f
unknown <ricardodonado1105@gmail.com>
2024-06-25 00:03:14 -0500

    verification scripted holy shit this took too long(nah i was not home)

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,40 @@
+
+
 const formElement = document.getElementsByTagName("form")[0]
 const sendButtonElement = document.getElementsByTagName("button")[0]
 
 
-// sendButtonElement.addEventListener("click", () => {
 function checkForm(){
     let allFilled = true
-
     for (let i = 0; i<formElement.length; i++){
         if (formElement[i].tagName === "INPUT" && formElement[i].value.trim() === ""){
             allFilled = false
+            break
+            }
         }
-    }
-    if (allFilled) {
-        sendButtonElement.disabled = false;
-    } else {
-        sendButtonElement.disabled = true;
-    }
-    for (let i = 0; i<formElement.length; i++){
-        if (formElement[i].tagName === "INPUT"){
-            formElement[i].addEventListener("input", checkForm)
-        }
+        if (allFilled) {
+            sendButtonElement.type = "submit";
+        } else {
+            sendButtonElement.type = "button";
     }
 }
+function clickOnSend(){
+    for (let i = 0; i<formElement.length; i++){
+        if (formElement[i].tagName === "INPUT" && formElement[i].value.trim() === ""){
+            formElement[i].focus()
+            break
+            }
+        }
+}
+for (let i = 0; i<formElement.length; i++){
+    if (formElement[i].tagName === "INPUT"){       
+        formElement[i].addEventListener("input", checkForm)
+    }
+}
+
+sendButtonElement.addEventListener("click", clickOnSend);
+
+let clicked = false;
+
+
 checkForm()
