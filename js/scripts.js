9c5842d97d9f085dcceac8bf1b386f6f211c53fe
unknown <ricardodonado1105@gmail.com>
2024-06-24 22:19:24 -0500

    scripts for the input filled validation

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
-console.log("hello world")
+const formElement = document.getElementsByTagName("form")[0]
+const sendButtonElement = document.getElementsByTagName("button")[0]
+
+
+// sendButtonElement.addEventListener("click", () => {
+function checkForm(){
+    let allFilled = true
+
+    for (let i = 0; i<formElement.length; i++){
+        if (formElement[i].tagName === "INPUT" && formElement[i].value.trim() === ""){
+            allFilled = false
+        }
+    }
+    if (allFilled) {
+        sendButtonElement.disabled = false;
+    } else {
+        sendButtonElement.disabled = true;
+    }
+    for (let i = 0; i<formElement.length; i++){
+        if (formElement[i].tagName === "INPUT"){
+            formElement[i].addEventListener("input", checkForm)
+        }
+    }
+}
+checkForm()
